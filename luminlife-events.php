7e140008b14e767fd4e5eef63a648f6f9254e6d7
Kiley Hykawy <kiley.hykawy@gmail.com>
2017-05-25 11:19:08 -0600

    Add support to include inline javascript function in versions of WordPress older than 4.5.

--- luminlife-events.php
+++ luminlife-events.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Lumin.Life Events
  * Plugin URI:        https://github.com/luminlife/wordpress
  * Description:       Shortcodes to include Lumin.life web widgets
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Lumin Arts Inc.
  * Author URI:        http://lumin.life
  * License:           GPL-2.0+
@@ -35,6 +35,74 @@ define("LUMINEVENTS_URL",
   "https://s3-us-west-2.amazonaws.com/cdn.lumin.life/widgets/v2/event.widget.min.js");
 define("LUMINEVENTS_STYLE_URL",
   "https://s3-us-west-2.amazonaws.com/cdn.lumin.life/widgets/v2/css/lumin_widget_styles.min.css");
+
+if (!function_exists('write_log')) {
+  function write_log($log) {
+    if (true === WP_DEBUG) {
+      if (is_array($log) || is_object($log)) {
+        error_log(print_r($log, true));
+      } else {
+        error_log($log);
+      }
+    }
+  }
+}
+
+/**
+ * Enqueue inline Javascript. @see wp_enqueue_script().
+ *
+ * KNOWN BUG: Inline scripts cannot be enqueued before
+ *  any inline scripts it depends on, (unless they are
+ *  placed in header, and the dependant in footer).
+ *
+ * @param string      $handle    Identifying name for script
+ * @param string      $src       The JavaScript code
+ * @param array       $deps      (optional) Array of script names on which this script depends
+ * @param bool        $in_footer (optional) Whether to enqueue the script before </head> or before </body>
+ *
+ * @return null
+ */
+function enqueue_inline_script( $handle, $js, $deps = array(), $in_footer = false ) {
+  // Callback for printing inline script.
+  $cb = function() use( $handle, $js ) {
+    // Ensure script is only included once.
+    if( wp_script_is( $handle, 'done' ) )
+      return;
+    // Print script & mark it as included.
+    echo "<script type=\"text/javascript\" id=\"js-$handle\">\n$js\n</script>\n";
+    global $wp_scripts;
+    $wp_scripts->done[] = $handle;
+  };
+  // (`wp_print_scripts` is called in header and footer, but $cb has re-inclusion protection.)
+  $hook = $in_footer ? 'wp_print_footer_scripts' : 'wp_print_scripts';
+
+  // If no dependencies, simply hook into header or footer.
+  if( empty($deps)) {
+    add_action( $hook, $cb );
+    return;
+  }
+
+  // Delay printing script until all dependencies have been included.
+  $cb_maybe = function() use( $deps, $in_footer, $cb, &$cb_maybe ) {
+    foreach( $deps as &$dep ) {
+      write_log("enqueue_inline_script: processing '${dep}'");
+      if( !wp_script_is( $dep, 'done' ) ) {
+        // Dependencies not included in head, try again in footer.
+        if( $in_footer ) {
+          add_action( 'wp_print_footer_scripts', $cb_maybe, 11 );
+        }
+        else {
+          // Dependencies were not included in `wp_head` or `wp_footer`.
+          write_log("enqueue_inline_script: '${dep}' not in head/footer");
+        }
+        return;
+      }
+    }
+    call_user_func( $cb );
+  };
+
+  add_action( $hook, $cb_maybe, 0 );
+}
 
 function lumin_events_shortcode($attributes) {
   /*
@@ -82,8 +150,16 @@ function lumin_events_shortcode($attributes) {
   $optionsStr .= "}";
 
   wp_enqueue_script('luminlife-events_scripts');
-  wp_add_inline_script('luminlife-events_scripts',
-    "luminEvents('${divId}', ${optionsStr});");
+
+  $inlineScript = "  luminEvents('${divId}', ${optionsStr});";
+  if (function_exists('wp_add_inline_script')) {
+    wp_add_inline_script('luminlife-events_scripts', $inlineScript);
+  } else {
+    enqueue_inline_script('luminlife-events_scripts_inline',
+      $inlineScript,
+      array('luminlife-events_scripts'),
+      true);
+  }
 
   $return_string = "<div id='${divId}'></div>";
   return $return_string;
